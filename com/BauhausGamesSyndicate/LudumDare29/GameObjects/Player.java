0ed634e0b08dd84b539c30ebf5ea6f563ba3741d
Benedikt Vogler <e-mail@benediktvogler.de>
2014-04-26 23:39:22 +0200

    improved underworld menu

--- com/BauhausGamesSyndicate/LudumDare29/GameObjects/Player.java
+++ com/BauhausGamesSyndicate/LudumDare29/GameObjects/Player.java
@@ -19,6 +19,7 @@ import com.badlogic.gdx.Input.Keys;
 public class Player extends AbstractCharacter {
     private boolean shouldRaise = false;
     private float speed=1/8f;
+    private int menupoint = 0;
     
     public Player(float x, float y) {
         super(x, y, "player");
@@ -28,34 +29,51 @@ public class Player extends AbstractCharacter {
     public void update(float delta){   
         super.update(delta);
         setAcceleration(0);
-        if (Gdx.input.isKeyPressed(Keys.D)){
-            setAcceleration(1);
-            //setX(getX()+speed*delta);
+        
+        if (GameScreen.onOverworld()){
+            if (Gdx.input.isKeyPressed(Keys.D)){
+                setAcceleration(1);
+                //setX(getX()+speed*delta);
+            }
+        
+            if (Gdx.input.isKeyPressed(Keys.A)){
+                setAcceleration(-1);
+                //setX(getX()-speed*delta);
+            }
+            
+            //go down?
+            Eingang eingang = GameScreen.getOverworld().getEingang();
+            if (
+                Gdx.input.isKeyPressed(Keys.S) &&
+                getX() > eingang.getX() &&
+                getX() < eingang.getX()+eingang.getWidth()
+                ){
+                GameScreen.switchWorld();
+                setX(860);
+                setY(500);
+            }
+        }else {
+            //move up?
+            if (Gdx.input.isKeyPressed(Keys.W)){
+                shouldRaise=true;
+            }
+            if (Gdx.input.isKeyPressed(Keys.D)){
+                goTo(3);
+            }
+            if (Gdx.input.isKeyPressed(Keys.S)){
+                goTo(2);
+            }
+            if (Gdx.input.isKeyPressed(Keys.A)){
+                goTo(1);
+            }
         }
         
-        if (Gdx.input.isKeyPressed(Keys.A)){
-            setAcceleration(-1);
-            //setX(getX()-speed*delta);
-        }
         setAcceleration(getAcceleration() * getAccFactor()    );
         setVelocity    (getVelocity()     + getAcceleration() );
         setVelocity    (getVelocity()     * (1 - getFriction()) );
         setX((getX() + getVelocity()*delta));
-        if (Gdx.input.isKeyPressed(Keys.W) && !GameScreen.onOverworld()){
-            shouldRaise=true;
-        }
         
-        Eingang eingang = GameScreen.getOverworld().getEingang();
-        if (
-            Gdx.input.isKeyPressed(Keys.S) &&
-            GameScreen.onOverworld()&&
-            getX()>eingang.getX() &&
-            getX()<eingang.getX()+eingang.getWidth()
-            ){
-            GameScreen.switchWorld();
-            setX(860);
-            setY(500);
-        }
+
         
         //switch
         if (shouldRaise && getY() >= Chunk.HEIGHT){
@@ -68,6 +86,23 @@ public class Player extends AbstractCharacter {
             setY(getY()+delta/2);
         }
              
+    }
+    
+    private void goTo(int id){
+        if (menupoint==0 &&  id==2){
+            setX(1000);
+            setY(350);
+        }else if (menupoint==0 && id==1){
+            setX(580);
+            setY(400);
+        } else if(menupoint==0 && id==3){
+            setX(1400);
+            setY(600);
+        } else if(menupoint==2 && id==0){
+            setX(860);
+            setY(500);
+        }
+        menupoint = id;
     }
     
 }
