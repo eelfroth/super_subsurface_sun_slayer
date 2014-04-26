13fa344064653997dab8abbfd2d991018155c217
Benedikt Vogler <e-mail@benediktvogler.de>
2014-04-26 22:56:07 +0200

    slower minion and enemy

--- com/BauhausGamesSyndicate/LudumDare29/GameObjects/Enemy.java
+++ com/BauhausGamesSyndicate/LudumDare29/GameObjects/Enemy.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package com.BauhausGamesSyndicate.LudumDare29.GameObjects;
 
 /**
@@ -14,7 +8,7 @@ public class Enemy extends AbstractCharacter {
 
     public Enemy(float x, float y) {
         super(x, y, "enemy");
-        setSpeed(0.3f);
+        setSpeed((float) (0.1f + Math.random()*.4f));
     }
 
     @Override
