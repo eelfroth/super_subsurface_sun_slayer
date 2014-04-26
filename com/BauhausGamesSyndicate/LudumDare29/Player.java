02d10379ac71baa27f33c8cb5aa1e55c4fec7122
Benedikt Vogler <e-mail@benediktvogler.de>
2014-04-26 16:21:03 +0200

    Added player

---
+++ com/BauhausGamesSyndicate/LudumDare29/Player.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.BauhausGamesSyndicate.LudumDare29;
+
+/**
+ *
+ * @author Benedikt Vogler
+ */
+public class Player extends AbstractCharacter {
+
+    public Player(float x, float y, String name) {
+        super(x, y, "player");
+    }
+    
+}
