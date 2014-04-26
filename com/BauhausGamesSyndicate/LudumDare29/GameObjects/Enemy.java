fa085c15820379fe8b8af5483e1d54abd8189ae8
PAFdelirium <paul.flechsig@aol.de>
2014-04-26 23:34:10 +0200

    enemy change direction

--- com/BauhausGamesSyndicate/LudumDare29/GameObjects/Enemy.java
+++ com/BauhausGamesSyndicate/LudumDare29/GameObjects/Enemy.java
@@ -5,16 +5,26 @@ package com.BauhausGamesSyndicate.LudumDare29.GameObjects;
  * @author Benedikt Vogler
  */
 public class Enemy extends AbstractCharacter {
-
+    public int direction;
     public Enemy(float x, float y) {
         super(x, y, "enemy");
+        direction = -1;
         setSpeed((float) (0.1f + Math.random()*.2f));
     }
 
     @Override
     public void update(float delta) {
+        int randomNum = 0 + (int)(Math.random()*80);
+        if(randomNum == 23){
+            direction *= -1;
+            if(direction == 1)
+                this.setFlip(true, false);
+            else
+                this.setFlip(false, false);
+        }
         super.update(delta);
-        setX(getX() - delta*getSpeed());//run to left
+        setX(getX() + direction*delta*getSpeed());//run to left
+        
     }
     
     
