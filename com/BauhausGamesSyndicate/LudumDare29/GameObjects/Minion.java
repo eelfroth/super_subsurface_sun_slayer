658728e9f8be1a11747e3eaa6bf0116b7b7dc348
Benedikt Vogler <e-mail@benediktvogler.de>
2014-04-26 22:29:58 +0200

    speed reduced

--- com/BauhausGamesSyndicate/LudumDare29/GameObjects/Minion.java
+++ com/BauhausGamesSyndicate/LudumDare29/GameObjects/Minion.java
@@ -1,12 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package com.BauhausGamesSyndicate.LudumDare29.GameObjects;
 
-import com.BauhausGamesSyndicate.LudumDare29.GameObjects.AbstractCharacter;
 
 
 /**
@@ -17,7 +10,7 @@ public class Minion extends AbstractCharacter{
     
     public Minion(float x){
         super(x, 0, "minion");
-        setSpeed(1f);
+        setSpeed(0.5f);
     }
     
 
