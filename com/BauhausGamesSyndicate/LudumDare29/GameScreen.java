08882c2f071f1b303dcdc3950eac559a17769f4c
eelfroth <jacob.bauer@uni-weimar.de>
2014-04-27 01:12:02 +0200

    ⋰┲┝┧∈⋰℉∐ℂ₭ℐՈ⅁⋰₴ҢꙘd℮ℜ⋰₩ö℟╳⋰

--- com/BauhausGamesSyndicate/LudumDare29/GameScreen.java
+++ com/BauhausGamesSyndicate/LudumDare29/GameScreen.java
@@ -20,6 +20,9 @@ import com.badlogic.gdx.graphics.glutils.FrameBuffer;
 import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Matrix4;
+import static java.lang.Math.floor;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GameScreen implements Screen {
     private final SpriteBatch batch;
@@ -39,10 +42,15 @@ public class GameScreen implements Screen {
     private FrameBuffer frameBuffer;
     private Mesh frameMesh;
     private Matrix4 worldMatrix;
+    private final Texture debug_texture;
+    private float world_streckfaktor;
+    private final float world_ypos;
 
     public GameScreen() {
         spritesheet = new TextureAtlas(Gdx.files.internal("com/BauhausGamesSyndicate/LudumDare29/assets/spritesheet.txt"));
-        overlay = new Texture(Gdx.files.internal("com/BauhausGamesSyndicate/LudumDare29/assets/overlay.png"));         
+        overlay = new Texture(Gdx.files.internal("com/BauhausGamesSyndicate/LudumDare29/assets/overlay.png"));
+        debug_texture = new Texture(Gdx.files.internal("com/BauhausGamesSyndicate/LudumDare29/assets/mapping.png"));
+        
         
         batch = new SpriteBatch();    
         font = new BitmapFont();
@@ -62,6 +70,10 @@ public class GameScreen implements Screen {
         underworld = new Underworld();
         
         player = new Player(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2);
+        
+        //parable settings:
+        world_streckfaktor = 0.25f;
+        world_ypos = -0.3f;
         
         //shader
         setupShader();
@@ -138,9 +150,12 @@ public class GameScreen implements Screen {
         
         //2. render framebuffer to frame;
         frameBuffer.getColorBufferTexture().bind();
+        //debug_texture.bind();
         shader.begin();
         shader.setUniformMatrix("u_worldView", worldMatrix);
         shader.setUniformi("u_texture", 0);
+        shader.setUniformf("f_stauchfaktor", world_streckfaktor);
+        shader.setUniformf("f_ypos", world_ypos);
         frameMesh.render(shader, GL20.GL_TRIANGLES);
         shader.end();
         
@@ -202,12 +217,77 @@ public class GameScreen implements Screen {
     
     private void setupFramebuffer() {
         frameBuffer = new FrameBuffer(Pixmap.Format.RGB565 , Gdx.graphics.getWidth(), Gdx.graphics.getWidth(), false);
-        frameMesh = new Mesh(true, 4, 6, VertexAttribute.Position(), VertexAttribute.ColorUnpacked(), VertexAttribute.TexCoords(0));
-        frameMesh.setVertices(new float[] 
-        {-1, -1, 0, 1, 1, 1, 1, 0, 0,
-          1, -1, 0, 1, 1, 1, 1, 1, 0,
-          1,  1, 0, 1, 1, 1, 1, 1, 1,
-         -1,  1, 0, 1, 1, 1, 1, 0, 1});
-        frameMesh.setIndices(new short[] {0, 1, 2, 2, 3, 0});
+        
+        //generate frameMesh
+        int xQuads = 20;
+        int yQuads = 20;
+
+        float[] vertices = new float[xQuads*yQuads*36];
+        short[] indices  = new short[xQuads*yQuads*6];
+        
+        float[] quadVertices = new float[36];
+        short[] quadIndices = new short[6];
+        
+        boolean backwards = true;
+        
+        for(int y = 0; y < yQuads; y++) {
+            for(int x = 0; x < xQuads; x++) {
+                if(backwards) {
+                    quadVertices = new float []
+                    {(x/(float)xQuads)                    *2-1, (y/(float)yQuads)                    *2-1, 0, 1, 1, 1, 1, (x/(float)xQuads)                    , (y/(float)yQuads),
+                     (x/(float)xQuads + (1/(float)xQuads))*2-1, (y/(float)yQuads)                    *2-1, 0, 1, 1, 1, 1, (x/(float)xQuads + (1/(float)xQuads)), (y/(float)yQuads),
+                     (x/(float)xQuads + (1/(float)xQuads))*2-1, (y/(float)yQuads + (1/(float)yQuads))*2-1, 0, 1, 1, 1, 1, (x/(float)xQuads + (1/(float)xQuads)), (y/(float)yQuads + (1/(float)yQuads)),
+                     (x/(float)xQuads)                    *2-1, (y/(float)yQuads + (1/(float)yQuads))*2-1, 0, 1, 1, 1, 1, (x/(float)xQuads)                    , (y/(float)yQuads + (1/(float)yQuads))};
+                }
+                System.arraycopy(quadVertices, 0, vertices, 36*(x+xQuads*y), 36);
+
+                short i = (short)((x+xQuads*y)*4);
+                quadIndices = new short[]
+                {(short) (0 + i), (short) (1 + i), (short) (2 + i), (short) (2 + i), (short) (3 + i), (short) (0 + i)};  
+                System.arraycopy(quadIndices, 0, indices, 6*(x+xQuads*y), 6);
+            }
+            //backwards = !backwards;
+        }
+        
+        frameMesh = new Mesh(true, xQuads*yQuads*4, xQuads*yQuads*6, VertexAttribute.Position(), VertexAttribute.ColorUnpacked(), VertexAttribute.TexCoords(0));
+        frameMesh.setVertices(vertices);
+        frameMesh.setIndices(indices);
+        /*frameMesh.setVertices(new float []
+                    {-0.1f-1, -0.1f, 0, 1, 1, 1, 1, 0, 0,
+                     0.1f-1, -0.1f, 0, 1, 1, 1, 1, 1, 0,
+                     0.1f-1, 0.1f, 0, 1, 1, 1, 1, 1, 1,
+                     -0.1f-1, 0.1f, 0, 1, 1, 1, 1, 0, 1});
+        frameMesh.setIndices(new short[] {0,1,2,2,3,0});*/
+        
+        //debug
+        StringBuffer result = new StringBuffer();
+        for (int i = 0; i < vertices.length; i++) {
+           if((i)%9 == 0) result.append("\n");
+           if((i)%36 == 0)  {
+               result.append("Quad ");
+               result.append(i/36);
+               result.append(":\n");
+           }
+           result.append( vertices[i] );
+           result.append("\t");
+           
+        }
+        String mynewstring = result.toString();
+        Gdx.app.log("frameMesh vertices:", mynewstring + "\n");
+        
+        result = new StringBuffer();
+        for (int i = 0; i < indices.length; i++) {
+           //if((i)%9 == 0) result.append("\n");
+           if((i)%6 == 0)  {
+               result.append("\nQuad ");
+               result.append(i/6);
+               result.append(":\n");
+           }
+           result.append( indices[i] );
+           result.append("\t");
+           
+        }
+        mynewstring = result.toString();
+        Gdx.app.log("frameMesh indices:", mynewstring);
     }
 }
--- com/BauhausGamesSyndicate/LudumDare29/shaders/world.vert
+++ com/BauhausGamesSyndicate/LudumDare29/shaders/world.vert
@@ -6,13 +6,15 @@ uniform mat4 u_worldView;
 varying vec4 v_color;
 varying vec2 v_texCoords;
 
-uniform float factor;
-varying vec4 displacement;
-
+varying vec4 v_parable;
+uniform float f_stauchfaktor;
+uniform float f_ypos;
 
 void main() {
     v_color = vec4(1, 1, 1, 1);
     v_texCoords = a_texCoord0;
-    displacement = vec4(a_position.y, 1, 1, 1);
-    gl_Position = u_worldView * a_position;
+
+    v_parable = vec4(0, (a_position.x*a_position.x * f_stauchfaktor) + f_ypos, 0, 0);
+    //a_position.y = v_parable;
+    gl_Position = u_worldView * (a_position - v_parable);
 }  
