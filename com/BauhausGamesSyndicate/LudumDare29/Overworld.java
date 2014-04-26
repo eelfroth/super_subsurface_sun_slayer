a2b3e960d74b4ba084cf60f59c7b1ae57a4de58b
Benedikt Vogler <e-mail@benediktvogler.de>
2014-04-26 13:11:43 +0200

    Added map background rendering

--- com/BauhausGamesSyndicate/LudumDare29/Overworld.java
+++ com/BauhausGamesSyndicate/LudumDare29/Overworld.java
@@ -1,7 +1,9 @@
 package com.BauhausGamesSyndicate.LudumDare29;
 
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
 import java.util.ArrayList;
 
 /**
@@ -14,21 +16,24 @@ public class Overworld {
     private int cameraPos = 0;
     private final int height =1080;
     private final int width = 16384;//2^14
+    private Texture graphic; 
 
     public Overworld() {
         this.heightmap = new int[100];
         for (int x = 0; x < heightmap.length/2; x++) {
             heightmap[x] = (int) (Math.random()*height);
         }
+        graphic = new Texture(Gdx.files.internal("com/BauhausGamesSyndicate/LudumDare29/map1.png"));
     }
     
     public void update(float delta){
-        cameraPos++;
+        cameraPos+=delta/5;
     }
     
     public void render(GameScreen gs){
        // sh.begin(ShapeRenderer.ShapeType.Filled);
         gs.getBatch().begin();
+        gs.getBatch().draw(graphic, -cameraPos, 0);
         
         //render map
 //        for (int x = 0; x < heightmap.length; x++) {
