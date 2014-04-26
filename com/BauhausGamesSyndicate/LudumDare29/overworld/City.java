bb56a782469ddada9f7a826768392659b0d6d1fe
Benedikt Vogler <e-mail@benediktvogler.de>
2014-04-26 21:33:49 +0200

    small changes

--- com/BauhausGamesSyndicate/LudumDare29/overworld/City.java
+++ com/BauhausGamesSyndicate/LudumDare29/overworld/City.java
@@ -1,9 +1,11 @@
 package com.BauhausGamesSyndicate.LudumDare29.overworld;
 
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
 /**
  *
  * @author Benedikt Vogler
  */
-public class City {
+public class City extends Sprite {
     
 }
