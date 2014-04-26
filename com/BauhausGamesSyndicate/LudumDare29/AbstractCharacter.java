fda4f61ae93e5459f9bd16b3107c4e91b7e13930
PAFdelirium <paul.flechsig@aol.de>
2014-04-26 16:59:53 +0200

    Charackter move attributes oder so

--- com/BauhausGamesSyndicate/LudumDare29/AbstractCharacter.java
+++ com/BauhausGamesSyndicate/LudumDare29/AbstractCharacter.java
@@ -5,24 +5,47 @@ import com.BauhausGamesSyndicate.LudumDare29.AbstractEntity;
         
 public abstract class AbstractCharacter extends AbstractEntity {
     public float speed;
+    public float acceleration;
+    public float accFactor;
+    public float velocity;
+    public static float friction;
+    
     public int life;
 
     public AbstractCharacter(float x, float y, String name){
         super(x, y, name);
-        life = 100;
+        life     = 100;
+        
+        speed    = 0;
+        velocity = 0;
+        accFactor = 0.2f;
+        acceleration = 0;
+        friction = 0.01f;
+        
     }
     
-    public int getLife(){
-        return life;
+    public float getVelocity(){
+        return this.velocity;
     }
     
-    public void set(int life){
-        this.life = life;
+    public void setVelocity(float v){
+        this.velocity = v;
     }
     
-    public boolean isDead(){
-        if(life <= 0) return true;
-        return false;
+    public float getAcceleration(){
+        return this.acceleration;
+    }
+    
+    public void setAcceleration(float acc){
+        this.acceleration = acc;
+    }
+    
+    public float getAccFactor(){
+        return this.accFactor;
+    }
+    
+    public void setAccFactor(float fac){
+        this.accFactor = fac;
     }
     
     public void setSpeed(float speed){
@@ -31,6 +54,19 @@ public abstract class AbstractCharacter extends AbstractEntity {
     
     public float getSpeed(){
         return speed;
+    }
+    
+    public int getLife(){
+        return life;
+    }
+    
+    public void setLife(int life){
+        this.life = life;
+    }
+    
+    public boolean isDead(){
+        if(life <= 0) return true;
+        return false;
     }
     
     public void update(float delta){
