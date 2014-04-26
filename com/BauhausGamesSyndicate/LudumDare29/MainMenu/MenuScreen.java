babef0318a4d8042f2171ec949cb5938aa0c8949
PAFdelirium <paul.flechsig@aol.de>
2014-04-26 21:03:49 +0200

    main menu

---
+++ com/BauhausGamesSyndicate/LudumDare29/MainMenu/MenuScreen.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.BauhausGamesSyndicate.LudumDare29.MainMenu;
+
+import com.BauhausGamesSyndicate.LudumDare29.GameScreen;
+
+/**
+ *
+ * @author Paul
+ */
+public class MenuScreen extends GameScreen{
+    public MenuScreen(){
+        setWorld(true);
+    }
+}
