5f882d31cafb46492e0e97a52363bca4a20afd85
Benedikt Vogler <e-mail@benediktvogler.de>
2014-04-26 11:15:41 +0200

    added dynamic tile size

--- com/BauhausGamesSyndicate/LudumDare29/Tile.java
+++ com/BauhausGamesSyndicate/LudumDare29/Tile.java
@@ -7,6 +7,7 @@
 package com.BauhausGamesSyndicate.LudumDare29;
 
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
 /**
  *
@@ -14,13 +15,17 @@ import com.badlogic.gdx.graphics.Color;
  */
 public class Tile {
    private Color color;
+   public static final int WIDTH = 20;
+   public static final int HEIGHT =20;
 
     public Tile(Color color) {
         this.color = color;
     }
 
-    public Color getColor() {
-        return color;
+
+    void render(ShapeRenderer sh, int xPos, int yPos) {
+        sh.setColor(color);
+        sh.rect(xPos, yPos, WIDTH, HEIGHT);
     }
    
    
