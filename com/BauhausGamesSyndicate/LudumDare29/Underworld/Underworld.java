035d55097e1fc3445568fd8ea1f254f430e0b719
Benedikt Vogler <e-mail@benediktvogler.de>
2014-04-26 15:42:20 +0200

    moved underworld and added underworld asset

---
+++ com/BauhausGamesSyndicate/LudumDare29/Underworld/Underworld.java
@@ -0,0 +1,30 @@
+package com.BauhausGamesSyndicate.LudumDare29.Underworld;
+
+import com.BauhausGamesSyndicate.LudumDare29.GameScreen;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+
+/**
+ *
+ * @author Benedikt Vogler
+ */
+public class Underworld {
+    private Texture texture;
+
+    public Underworld() {
+        this.texture = new Texture(Gdx.files.internal("com/BauhausGamesSyndicate/LudumDare29/assets/underworld.jpg"));
+    }
+    
+    
+    /**
+     * Etwas was passieren sollen wenn die Utnerwelt betreten wird.
+     */
+    public void enter(){
+    
+    }
+    
+    public void render(GameScreen gs){
+        gs.getBatch().draw(texture, 0, 0);
+    }
+    
+}
