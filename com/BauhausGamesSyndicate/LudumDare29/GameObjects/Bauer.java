0f55a66ac34a4a0515bac2a06ee76e6333becbb9
Benedikt Vogler <e-mail@benediktvogler.de>
2014-04-27 17:52:44 +0200

    added bauer

---
+++ com/BauhausGamesSyndicate/LudumDare29/GameObjects/Bauer.java
@@ -0,0 +1,15 @@
+package com.BauhausGamesSyndicate.LudumDare29.GameObjects;
+
+import com.BauhausGamesSyndicate.LudumDare29.overworld.Eingang;
+
+/**
+ *
+ * @author Benedikt Vogler
+ */
+public class Bauer extends Enemy {
+
+    public Bauer(float x, float y, boolean world, Eingang eingang) {
+        super(x, y, world, eingang);
+    }
+    
+}
