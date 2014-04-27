ace97b2b13328593fcb4f9b671577349d43d27b2
Benedikt Vogler <e-mail@benediktvogler.de>
2014-04-27 19:09:40 +0200

    special animation needs an "s" as suffix

--- com/BauhausGamesSyndicate/LudumDare29/GameObjects/AbstractEntity.java
+++ com/BauhausGamesSyndicate/LudumDare29/GameObjects/AbstractEntity.java
@@ -61,7 +61,7 @@ public abstract class AbstractEntity{
         specialTextures = new TextureRegion[specialSteps];
             
          for (int i = 0; i < specialSteps; i++) {
-            specialTextures[i] = GameScreen.getSpritesheet().findRegion(name+""+Integer.toString(i)); 
+            specialTextures[i] = GameScreen.getSpritesheet().findRegion(name+""+Integer.toString(i)+"s"); 
             if (specialTextures[i]==null)
                 System.err.println(name+""+Integer.toString(i));
         }
