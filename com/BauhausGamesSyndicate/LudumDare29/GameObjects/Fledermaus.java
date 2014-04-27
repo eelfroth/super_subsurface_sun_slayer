be0520ec79be23c900d31cd58b08369abfb7008d
eelfroth <jacob.bauer@uni-weimar.de>
2014-04-28 00:23:24 +0200

    The bat has left the belltower.

--- com/BauhausGamesSyndicate/LudumDare29/GameObjects/Fledermaus.java
+++ com/BauhausGamesSyndicate/LudumDare29/GameObjects/Fledermaus.java
@@ -1,6 +1,9 @@
 package com.BauhausGamesSyndicate.LudumDare29.GameObjects;
 
 import com.BauhausGamesSyndicate.LudumDare29.GameScreen;
+import com.badlogic.gdx.Gdx;
+import static java.lang.Math.random;
+import static java.lang.Math.sin;
 
 /**
  *
@@ -9,6 +12,9 @@ import com.BauhausGamesSyndicate.LudumDare29.GameScreen;
  * @author Jacob Bauer
  */
 public class Fledermaus extends Minion{
+    float y_sin;
+    float x_sin;
+    float y_pos;
 
     public Fledermaus(boolean world) {
        super(
@@ -19,10 +25,42 @@ public class Fledermaus extends Minion{
            4,
            1
        );
+       
+       //startwerte zugällig, damit die fledermäuse unterschiedlich fliegen;
+       y_sin = ((float)Math.random())*1000f;
+       x_sin = ((float)Math.random())*1000f;
+       y_pos = ((float)Math.random())*100f;
     }  
 
     @Override
     public void fight(AbstractCharacter enemy, float delta) {
         //playSpacial(true);
+    }
+    
+    @Override
+    public void update(float delta) {
+        super.update(delta);
+        
+        //(debug) unendlich leben
+        setLife(1003577);
+        
+        //den x- und y-koordinaten werden sinuskurven addiert
+        y_sin += delta*0.167;
+        setY((getY() + (float)sin(y_sin/147f)*200.0f) + 150.0f + y_pos);
+        x_sin += delta*0.167;
+        setX(getX() + (float)sin(x_sin/211f)*11f);
+        
+        
+        //debug shit
+        boolean debug = false;
+        if(debug) {
+            StringBuffer result = new StringBuffer();
+            result.append("\t");
+            result.append(y_sin);
+            result.append("\t");
+            result.append((float)sin(y_sin/100)*100);
+            String mynewstring = result.toString();
+            Gdx.app.log("", mynewstring);
+        }
     }
 }
