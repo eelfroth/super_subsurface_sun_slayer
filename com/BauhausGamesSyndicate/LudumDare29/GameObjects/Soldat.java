73c5ff210eb7e1418ec2329a053c93d9b2b68718
PAFdelirium <paul.flechsig@aol.de>
2014-04-28 04:39:19 +0200

    soldate update

--- com/BauhausGamesSyndicate/LudumDare29/GameObjects/Soldat.java
+++ com/BauhausGamesSyndicate/LudumDare29/GameObjects/Soldat.java
@@ -15,7 +15,7 @@ public class Soldat extends AbstractCharacter {
     private int dTimer;
     private int dTimerMax = 500;
     private float homeX;
-    private float reach = 500;
+    private float reach = 600;
     
     public Soldat(float x, float y, boolean world) {
         super(x, y, "soldat", world,2,1);
@@ -24,6 +24,7 @@ public class Soldat extends AbstractCharacter {
         homeX = x;
         dTimer = 0;
         setFriction(0.5f);
+        setAccFactor(getAccFactor() + (float) (Math.random()*0.1));
         //setAccFactor(0.03f + (int)Math.random()*0.5f);
         //setSpeed((float) (0.1f + Math.random()*.2f));
         if( (Math.random()*2)-1 > 0)
@@ -44,14 +45,14 @@ public class Soldat extends AbstractCharacter {
         }
         */
         if(getX() < homeX - reach/2 ||
-           getX() > homeX + reach){
+           getX() > homeX + reach/2){
            if(getX() > homeX)
                setAcceleration(-1);
            else
                setAcceleration(1);
         }
         if(GameScreen.getPlayer().getX() > homeX - reach/2 && // wenn player in Heimat eindringt
-           GameScreen.getPlayer().getX() < homeX + reach){
+           GameScreen.getPlayer().getX() < homeX + reach/2){
            setX(getX() + getAcceleration()*2);
            if(GameScreen.getPlayer().getX() > getX())
                setAcceleration(1);
