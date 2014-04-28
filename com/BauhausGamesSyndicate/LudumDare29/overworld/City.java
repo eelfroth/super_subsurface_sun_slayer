1853536f20a4a95eb81e6edfa307a66af1723751
PAFdelirium <paul.flechsig@aol.de>
2014-04-28 20:54:20 +0200

    some spwaning changes

--- com/BauhausGamesSyndicate/LudumDare29/overworld/City.java
+++ com/BauhausGamesSyndicate/LudumDare29/overworld/City.java
@@ -9,36 +9,10 @@ import com.BauhausGamesSyndicate.LudumDare29.GameObjects.Bauer;
  * @author Paul Flechsig
  * @author Jacob Bauer
  */
-public class City extends AbstractEntity {
-    private float timetillspawn = 1000;
-    private float timer;
-    private final Overworld overworld;
-    
-    public City(Overworld overworld, int x, int y) {
-        super(x, y, "bauernhof", true, 1,1);  
-
-        if((int)(Math.random()*10) > 5){
-            this.setFlipHorizontal(true);
-        }
-        this.overworld = overworld;
-    }
-    
-    @Override
-    public void update(float delta){
-        timer+=delta;
-        if (timer>=timetillspawn) {
-            timer = 0;
-            spawnEnemy();
-        }
-    }
-
-    private void spawnEnemy() {
-       /*
-          100% Bauern 
-          Quantity: 1
-       */
-       Bauer enemy = new Bauer(getX(),getY(), onOverworld());
-
-       overworld.addEntity(enemy);
+public class City extends AbstractSpawn{
+    public City(Overworld overworld, int x, int y){
+        super(overworld, x, y, "city");
+        setOverallQuantity(Tuning.CITY_SPAWN_QUANTITY);
+        setBQuantity(Tuning.CITY_SPAWN_BAUER_RATE);
     }
 }
